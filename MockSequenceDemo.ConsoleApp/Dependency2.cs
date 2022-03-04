acffda903f8d78749048427efdb5730d6c6b5d1e
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-03-04 15:55:31 +0530

    Created the console app

---
+++ MockSequenceDemo.ConsoleApp/Dependency2.cs
@@ -0,0 +1,6 @@
+﻿namespace MockSequenceDemo.ConsoleApp;
+
+internal class Dependency2 : IDependency2
+{
+    void IDependency2.DoWork() => Console.WriteLine("Dependency2 dowork");
+}
---
+++ MockSequenceDemo.ConsoleApp/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MockSequenceDemo.ConsoleApp;
+
+internal static class StartUp
+{
+    public static IServiceProvider ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        services.AddTransient<IDependency1, Dependency1>();
+        services.AddTransient<IDependency2, Dependency2>();
+        services.AddTransient<IWorker, Worker>();
+
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+        return serviceProvider;
+    }
+}
